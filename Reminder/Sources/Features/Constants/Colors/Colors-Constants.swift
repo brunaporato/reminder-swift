31732a92cfb6b657b70a21855c201834155d86ab
brunaporato <brunaporato@gmail.com>
2025-10-25 12:32:53 -0300

    feat: create splash screen

---
+++ Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Colors-Constants.swift
+//  Reminder
+//
+//  Created by Bruna Porato on 25/10/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Colors {
+    static let redBase = UIColor(red: 0.75, green: 0.15, blue: 0.21, alpha: 1)
+    static let redLight = UIColor(red: 0.92, green: 0.24, blue: 0.3, alpha: 1)
+    static let blueBase = UIColor(red: 0.2, green: 0.31, blue: 0.86, alpha: 1)
+    static let blueLight = UIColor(red: 0.43, green: 0.51, blue: 0.93, alpha: 1)
+}
---
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Metrics-Constants.swift
+//  Reminder
+//
+//  Created by Bruna Porato on 25/10/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Metrics {
+    static let huge: CGFloat = 48
+    static let medium: CGFloat = 24
+    static let small: CGFloat = 12
+    static let tiny: CGFloat = 8
+    static let little: CGFloat = 4
+}
