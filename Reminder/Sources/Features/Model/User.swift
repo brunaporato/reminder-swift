18301c02c47ae1f473f347d843a4996abeb9917d
brunaporato <brunaporato@gmail.com>
2025-11-04 08:32:16 -0300

    wip:feat: create userDefaults

---
+++ Reminder/Sources/Features/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Reminder
+//
+//  Created by Bruna Porato on 04/11/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    let email: String
+    let isUserSaved: Bool
+}
