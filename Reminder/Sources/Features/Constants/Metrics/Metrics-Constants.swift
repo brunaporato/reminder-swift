0918d1e6c988907c8a0d9cf2ca52c3b9c4edd763
brunaporato <brunaporato@gmail.com>
2025-11-22 10:25:33 -0300

    feat: create buttonHome component

--- Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -12,6 +12,7 @@ public struct Metrics {
     static let huge: CGFloat = 48
     static let large: CGFloat = 32
     static let medium: CGFloat = 24
+    static let medier: CGFloat = 16
     static let small: CGFloat = 12
     static let tiny: CGFloat = 8
     static let little: CGFloat = 4
